f1fcbc3f21a14b28707677e6cb1559d9f0354697
88ee55 <88ee55@gmail.com>
2016-12-25 14:32:13 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #from wsgiref.simple_server import make_server
 #from cgi import parse_qs, escape
-def application(env, start_response):
+def app(env, start_response):
     qs=environ['QUERY_STRING']
     otvet=qs.replace("&",r"\r\n")
     start_response('200 OK', [('Content-Type','text/plain')])
