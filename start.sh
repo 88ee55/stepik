38d5466da8290ddc861b0169377194a25f3ad6ef
88ee55 <88ee55@gmail.com>
2016-12-25 13:46:03 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ mkdir public/css
 mkdir public/js
 mkdir uploads
 mkdir etc
-cp ../stepik/nginx.conf etc/
+cd ..
+cp stepik/nginx.conf etc/
+cp stepik/hello.py web/
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my.conf
 
