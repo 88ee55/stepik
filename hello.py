13de2aa589b8b72ed448115c2ef2419258f2ae92
88ee55 <88ee55@gmail.com>
2016-12-25 15:13:08 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@
 from wsgiref.simple_server import make_server
 from cgi import parse_qs, escape
 def app(env, start_response):
-    qs = parse_qs(env['QUERY_STRING'])
-    for key in qs:
-        otvet=otvet+key+"="+qs[key]+"\r\n"
+#    qs = parse_qs(env['QUERY_STRING'])
+#    for key in qs:
+#        otvet=otvet+key+"="+qs[key]+"\r\n"
+    qs=(env['QUERY_STRING'])
+    otvet=qs.replace("&","\n")
     start_response('200 OK', [('Content-Type','text/plain')])
     #return [b"Hello World"] # python3
     return otvet # python2
