4cf2703b7a9dc95a5121c55e287dcd65c5cbd9a5
88ee55 <88ee55@gmail.com>
2016-12-25 12:10:15 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Go"
-chmod a+x update.sh
-mkdir ../web/
-cd ../web/
+chmod a+x /stepik/update.sh
+mkdir web
+cd web/
 mkdir public
 mkdir public/img
 mkdir public/css
 mkdir public/js
 mkdir uploads
 mkdir etc
-cp ../stepik/nginx.conf etc/
+cp /stepik/nginx.conf etc/
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my.conf
 
