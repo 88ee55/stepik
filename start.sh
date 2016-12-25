322c6ed99999f5e30a73c810645a0c29e341a500
88ee55 <88ee55@gmail.com>
2016-12-25 12:02:46 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,5 +10,5 @@ mkdir public/js
 mkdir uploads
 mkdir etc
 cp ../stepik/nginx.conf etc/
-ln -s /home/box/web/etc /etc/nginx/sites-enabled/my.conf
+ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/my.conf
 
