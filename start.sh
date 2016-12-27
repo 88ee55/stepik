ef8632dcd6d95553aac9a115f972ba41b317b116
88ee55 <88ee55@gmail.com>
2016-12-27 16:33:31 +0500

    Update

--- start.sh
+++ start.sh
@@ -16,5 +16,17 @@ sudo unlink /etc/nginx/sites-enabled/default
 cp stepik/hello.py web/
 cp stepik/hello.py.conf web/etc/hello.py
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo rm -rf /etc/gunicorn.d/
+sudo rm -rf /etc/gunicorn.d/*.example
+cd web/
+django-admin startproject ask
+cd ask/
+django-admin startapp qa
+cd qa/
+echo "from django.http import HttpResponse" >> views.py
+echo "def test(request, *args, **kwargs):" >> views.py
+echo -e "\treturn HttpResponse('OK')" >> views.py
+cd ../../../
+cp stepik/urls.py web/ask/ask/
+chown box.box web/ask/ask/urls.py
+
 
